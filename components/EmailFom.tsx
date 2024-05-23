6a52efffdd96f3e83962376f9c99dc04983de809
usaydafzal <142539380+usaydafzal@users.noreply.github.com>
2024-05-23 10:33:32 +0530

    Update EmailFom.tsx

--- components/EmailFom.tsx
+++ components/EmailFom.tsx
@@ -1,10 +1,9 @@
-"use client";
 import { InfoCircledIcon } from "@radix-ui/react-icons";
 import { useState } from "react";
 import { toast } from "react-hot-toast";
 
 export default function EmailForm() {
-  const [email, setEmail] = useState<string>();
+  const [email, setEmail] = useState("");
 
   const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setEmail(e.target.value);
@@ -12,41 +11,30 @@ export default function EmailForm() {
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-
-    try {
-      const response = await fetch("/api/submit", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ email }),
-      });
-
-      if (response.ok) {
-        setEmail("");
-        toast.success("Thank you for joining our waitlist! 🚀");
-      } else {
-        setEmail("");
-        toast.error("Oops! Something went wrong!");
-      }
-    } catch (err) {
-      setEmail("");
-      console.error(err);
-    }
+    
+    // As you are submitting to an external service, you might omit the fetch block
+    // that sends data to your API endpoint and handle only the Mailchimp form processing
   };
+
   return (
     <>
-      <form onSubmit={handleSubmit} method="POST" className="mt-2 max-w-sm">
+      <form 
+        onSubmit={handleSubmit} 
+        action="https://wearably.us22.list-manage.com/subscribe/post?u=46cc38378ffa4772023b50e4f&id=eaf7242092&f_id=008cc9e1f0" 
+        method="post" 
+        id="mc-embedded-subscribe-form" 
+        name="mc-embedded-subscribe-form" 
+        className="validate mt-2 max-w-sm"
+        target="_self"
+        noValidate
+      >
         <div className="flex flex-col gap-2 lg:flex-row">
-          <label className="sr-only" htmlFor="email-address">
-            Email address
-          </label>
           <input
             autoComplete="email"
             className="text-accent-500 block h-10 w-full focus:invalid:border-red-400 focus:invalid:text-red-500 focus:invalid:ring-red-500 appearance-none rounded-lg border-2 border-slate-300 px-4 py-2 placeholder-zinc-400 duration-200 focus:outline-none focus:ring-zinc-300 sm:text-sm"
             pattern="[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,}$"
-            id="email-address"
-            name="email"
+            id="mce-EMAIL"
+            name="EMAIL"
             placeholder="johndoe@example.com"
             required
             type="email"
@@ -60,12 +48,15 @@ export default function EmailForm() {
             <span>Join the waitlist</span>
           </button>
         </div>
+        <div style={{ position: "absolute", left: "-5000px", ariaHidden: "true"}}>
+          <input type="text" name="b_46cc38378ffa4772023b50e4f_eaf7242092" tabIndex="-1" value="" />
+        </div>
       </form>
 
       <div className="flex items-start gap-2 text-gray-500">
         <InfoCircledIcon />
         <p className="text-xs -mt-[0.5] max-w-sm">
-          No worries! your data is completely safe and will only be utilized to
+          No worries! Your data is completely safe and will only be utilized to
           provide you with updates about our product.
         </p>
       </div>
